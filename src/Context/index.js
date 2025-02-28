7926ac34a2e33374835b7e062153c1afdd2fd48a
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-02-28 21:15:02 +0530

    Commit from developer

---
+++ src/Context/index.js
@@ -0,0 +1,5 @@
+export { TodoContext , TodoProvider , useTodo} from "./TodoContext"
+
+
+
+//this file is created to manage imports and exports at a single place , this will help at the time of multiple contexts.
